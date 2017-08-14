967218b27135ab912223315a7d5b1509c39b9a7b
Louis <louisaleksieb.dagusen@gmail.com>
2017-08-14 08:30:09 +0800

    added relations ManyToMany in film -addfield actor to film and add field genre to film

--- Movie/admin.py
+++ Movie/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Film
+from .models import Film, Actor, Genre
 # Register your models here.
 
 admin.site.register(Film)
+admin.site.register(Actor)
+admin.site.register(Genre)

4407e9fd50d123f5e6edf461bb76391b3412de08
Louis <louisaleksieb.dagusen@gmail.com>
2017-08-14 08:13:48 +0800

    add to models.py class Films

--- Movie/models.py
+++ Movie/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Film(models.Model):
+	Title = models.CharField(max_length=150)
+	Description = models.TextField()
+	Year = models.DateTimeField('Date Published')
+
+	def __str__(self):
+		return self.Title
